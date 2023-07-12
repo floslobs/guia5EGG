a6ce84dd2c57e3682ed6604e86c51b7fbddb0734
floslobs <fan1_avril@hotmail.com>
2023-07-12 15:50:44 -0300

    ejej2

--- src/guia5flor/ejej2.java
+++ src/guia5flor/ejej2.java
@@ -5,6 +5,8 @@
  */
 package guia5flor;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ARG
@@ -16,6 +18,32 @@ public class ejej2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*Realizar un algoritmo que llene un vector de tamaño N con valores 
+        aleatorios y le pida al usuario un número a buscar en el vector. El 
+        programa mostrará dónde se encuentra el numero y si se encuentra repetido
+         */
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        int vec[] = new int[20];
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = (int) (Math.random() * 20);
+
+        }
+        for (int i = 0; i < vec.length; i++) {
+            System.out.println(vec[i]);
+
+        }
+        System.out.println("que numero desea encontrar?");
+        int num = sc.nextInt();
+
+        for (int i = 0; i < vec.length; i++) {
+            if (num == vec[i]) {
+
+                System.out.println("el numero se encontro en la posicion " + i);
+
+            }
+
+        }
+
     }
-    
+
 }
